74f3e3639e361458dc9a09f6433cf6ec40a8e222
ongian <ongchristian117@gmail.com>
2022-03-06 07:55:31 +0800

    move up the  serve static above middlewares

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const app = express();
 
 connectDB();
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 //Init Middleware
 app.use(express.json())
 
@@ -14,15 +24,7 @@ app.use('/api/profile/', require('./routes/api/profile'));
 app.use('/api/post/', require('./routes/api/post'));
 app.use('/api/auth/', require('./routes/api/auth'));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+
 
 const PORT = process.env.PORT || 5000;
 
