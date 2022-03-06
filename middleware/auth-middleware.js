73d33bc9c4028366822a9e978bfca73b95ff399b
ongian <ongchristian117@gmail.com>
2022-03-06 14:18:48 +0800

    replace auth middlewares jwtsecret

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,7 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-const config = require('config')
-
 module.exports = function(req, res, next) {
     //Get token from header
     const token = req.header('x-auth-token');
@@ -13,7 +11,7 @@ module.exports = function(req, res, next) {
     //Decode token if has
   // Verify token
   try {
-    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+    jwt.verify(token, process.env.jwtSecret, (error, decoded) => {
       if (error) {
         return res.status(401).json({ msg: 'Token is not valid' });
       } else {
